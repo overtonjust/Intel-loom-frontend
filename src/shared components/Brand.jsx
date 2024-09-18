545a29cb3e825fed5fb2024b2ab54a2a58644e16
marinn6 <nmarin@pursuit.org>
2024-09-18 16:14:58 -0400

    Add: Brand Component

---
+++ src/shared components/Brand.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Brand = () => {
+  return (
+    <h1>Intel Loom</h1>
+  )
+}
+
+export default Brand
---
+++ src/shared components/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div className='user-container'>User
+    
+    </div>
+    
+  )
+}
+
+export default User
