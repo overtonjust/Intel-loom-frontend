7f193f511fc512ae37a27f5623d94d0b48a512eb
marinn6 <nmarin@pursuit.org>
2024-09-19 16:15:40 -0400

    rebase changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // Dependencies
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
+import { Route, Routes } from 'react-router-dom';
+import UserPage from './pages/UserPage';
 
 // Layouts
 import DefaultLayout from './layouts/DefaultLayout';
@@ -15,9 +15,14 @@ import Conference from './pages/conference/Conference';
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<DefaultLayout />}>
+      <Route path='/' element={<DefaultLayout/>}>
+      <Route path='/users' element={<UserPage/>}></Route>
       </Route>
+      
+
+   
     </Routes>
+
   );
 };
 
