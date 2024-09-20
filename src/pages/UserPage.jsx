f92714e6d2324d521fa63b58b44ee02bc1a0e97a
marinn6 <nmarin@pursuit.org>
2024-09-20 16:07:29 -0400

    Update: UserPage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { FaLinkedin, FaYoutube } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 const UserPage = () => {
   return (
-    <div>UserPage</div>
+    
+    <div className='user_container'>UserPage
+    
+        <div className='user_profile-img' alt='profile-img'>
+            <img src="https://placehold.co/200x280" alt="profile image" />
+        </div>
+        <div className='user_name'>First Last</div>
+        <div className='user_bio'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non scelerisque nunc. Integer tristique pretium porta.</div>
+        <div className='user_socials'>
+            <button><FaLinkedin /></button> 
+            <button><MdOutlineEmail /></button> 
+            <button><FaYoutube /></button>
+        </div>
+
+        <div className='user_videos'>Videos:
+            <img className='user_video1' src="https://placehold.co/300x200" alt="video1" />
+            <img className='user_video2' src="https://placehold.co/300x200" alt="video2" />
+            <img className='user_video3' src="https://placehold.co/300x200" alt="video3" />
+
+        </div>
+    </div>
   )
 }
 
