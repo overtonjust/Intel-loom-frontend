ea9f69dc24c566d4dc503299304353df2e6c1f37
overtonjust <joverton@pursuit.org>
2024-09-20 11:35:48 -0400

    sync dev with team 9/20

--- src/pages/conference/Conference.jsx
+++ src/pages/conference/Conference.jsx
@@ -8,16 +8,10 @@ import {
 import './Conference.scss';
 
 // Components
-<<<<<<< HEAD
 import JoinCall from './components/JoinCall';
 import ConferenceRoom from './components/ConferenceRoom';
 import MenuOptions from './components/MenuOptions';
 import ConferenceInfo from './components/ConferenceInfo';
-=======
-import JoinCall from './components/JoinCall'
-import ConferenceRoom from './components/ConferenceRoom'
-import MenuOptions from './components/MenuOptions';
->>>>>>> b2f3be6 (added MenuOptions info and package-lock to .gitign)
 
 const Conference = ({/** Grab room / class data */}) => {
     const isConnected = useHMSStore(selectIsConnectedToRoom);
@@ -36,13 +30,7 @@ const Conference = ({/** Grab room / class data */}) => {
                 <article className='conference__ video-call'>
                     <ConferenceRoom/>
                     <MenuOptions/>
-<<<<<<< HEAD
                 </article>
-=======
-                    {/* add ConferenceRoom component to hold all each videoPlayer component
-                        add RoomMenu component to house all options (ie: mute, show video, chat etc) */}
-                </>
->>>>>>> b2f3be6 (added MenuOptions info and package-lock to .gitign)
             ): (
                 <JoinCall/>
             )}
--- src/pages/conference/components/MenuOptions.jsx
+++ src/pages/conference/components/MenuOptions.jsx
@@ -36,7 +36,6 @@ const MenuOptions = () => {
     } = useAVToggle();
 
     return (
-<<<<<<< HEAD
         <main className='menu-holder'>
             <section className='menu-options-head'>
                 <article onClick={toggleAudio} className='menu-options-head__audio'>
@@ -104,80 +103,6 @@ const MenuOptions = () => {
                 </button>
             )}
         </main>
-=======
-        <>
-            <div className='main-control'>
-                <div onClick={toggleAudio} className='main_controls_button'>
-                    {isLocalAudioEnabled ? (
-                        <>
-                            <FontAwesomeIcon icon={faMicrophoneSlash} size="lg"/>
-                            <span className='button_name'>Mute</span>
-                        </>
-                    ) : (
-                        <>
-                            <FontAwesomeIcon icon={faMicrophone} size="lg"/>
-                            <span className='button_name'>Unmute</span>
-                        </>
-                    )}
-                </div>
-                <div onClick={toggleVideo} className='main_controls_button'>
-                    {isLocalVideoEnabled ? (
-                        <>
-                            <FontAwesomeIcon icon={faVideoSlash} size="lg"/>
-                            <span className='button_name'>Stop Video</span>
-                        </>
-                    ) : 
-                        <>
-                            <FontAwesomeIcon icon={faVideo} size="lg"/>
-                            <span className='button_name'>Start Video</span>
-                        </>
-                    }
-                </div>
-
-            </div>
-            <div className='main_controls_section'>
-                <div className='main_controls_button'>
-                    <div>
-                        <FontAwesomeIcon icon={faUserPlus} size='lg'/><>{userCount}</>
-                    </div>
-                    <span className='button_name'>Participants</span>
-                </div>
-                <div className='main_controls_button'>
-                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} size='lg' color=""/>
-                    <span className='button_name'>Share Screen</span>
-                </div>
-                <div className='main_controls_button'>
-                    <FontAwesomeIcon icon={faMessage} size='lg'/>
-                    <span className='button_name'>Chat</span>
-                </div>
-                <div className='main_controls_button'>
-                    <FontAwesomeIcon icon={faFaceSmile} size='lg'/>
-                    <span className='button_name'>Reactions</span>
-                </div>
-                <div className='main_controls_button'>
-                    <FontAwesomeIcon icon={faGear} size='lg'/>
-                    <span className='button_name'>Settings</span>
-                </div>
-                <div className='main_controls_button'>
-                    <FontAwesomeIcon icon={faEllipsis} size='lg'/>
-                    <span className='button_name'>More</span>
-                </div>
-            </div>
-            <div className='main_controls_section'>
-                <div className='main_controls_button'>
-                    {isConnected && (
-                        <button
-                        id='leave-btn'
-                        class='btn-danger'
-                        onClick={() => hmsActions.leave()}
-                        >
-                            Leave
-                        </button>
-                    )}
-                </div>
-            </div>
-        </>
->>>>>>> b2f3be6 (added MenuOptions info and package-lock to .gitign)
     );
 };
 
