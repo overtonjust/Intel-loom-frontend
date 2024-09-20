ce60fa0bfdaaef661973f401219823339524723d
overtonjust <joverton@pursuit.org>
2024-09-20 11:48:43 -0400

    Created PopUp & SearchBar components + Updated App.jsx + DefaultLayout.jsx + Updated .json with libraries for styling.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import UserPage from './pages/UserPage';
 import DefaultLayout from './layouts/DefaultLayout/DefaultLayout';
 
 // Components
+import SearchBar from './layouts/components/SearchBar';
 
 // Pages
 import Conference from './pages/conference/Conference';
