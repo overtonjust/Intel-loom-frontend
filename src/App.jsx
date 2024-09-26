9086c10e023ab324c8a874ec7b3d7c0b478d2b74
Erimed33 <erikamedina@pursuit.org>
2024-09-26 06:30:26 -0400

    classpage-setup

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Home from './pages/home/Home';
 import UserPage from './pages/UserPage/UserPage';
 import Login from './pages/login/Login';
 import Conference from './pages/conference/Conference';
+import ClassPage from './pages/classpage/ClassPage'
 
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
         <Route index element={<Home/>}/>
         <Route path='/profile/:id' element={<UserPage/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/classInfo/:id' element={<ClassPage />} />
         <Route path='/view' element={<Conference/>}/>
       </Route>  
     </Routes>
--- src/pages/classpage/ClassPage.jsx
+++ src/pages/classpage/ClassPage.jsx
@@ -1,20 +1,38 @@
-import { useEffect, useState, useContext } from "react"
-import { useParams } from "react-router-dom"
-import axios from "axios"
-import { UserContext } from "../../context/UserContext"
+import { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { UserContext } from "../../context/UserContext";
+import ClassCard from '../../shared components/ClassCard';
+import './ClassPage.scss'
 
 const ClassPage = () => {
-  const { API } = useContext(UserContext)
-  const { id } = useParams()
-  const [classData, setClassData] = useState(null)
+  const { API } = useContext(UserContext);
+  const { id } = useParams();
+  const [classData, setClassData] = useState(null);
 
   useEffect(() => {
-    axios.get(`${API}/classInfo/${id}`)
+    axios.get(`${API}/classes/classInfo/${id}`)
       .then(res => setClassData(res.data))
-      .catch(err => console.log(err))
-  }, [])
+      .catch(err => console.log(err));
+  }, [API, id]);
 
-  return null
+  if (!classData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main className="class-container">
+  <div className="title-button-container">
+    <h1 className="title-container">{classData.title}</h1>
+    <button className="book-now-button">Book Now</button>
+  </div>
+  <ClassCard key={classData.classId} classInfo={classData} />
+  <div className="description-container">{classData.description}</div>
+  <div className="instructor-bio-container">{classData.bio}</div>
+</main>
+
+  );
 }
 
-export default ClassPage
+export default ClassPage;
+
