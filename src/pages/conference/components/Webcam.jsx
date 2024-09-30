3c949b9f8f2f6d87705a008007c93759e11656d9
overtonjust <joverton@pursuit.org>
2024-09-30 11:04:23 -0400

    draft cam placeholder img

--- src/pages/conference/components/Webcam.jsx
+++ src/pages/conference/components/Webcam.jsx
@@ -1,28 +1,32 @@
 // Dependencies
 import React, { useContext } from 'react';
-import { useVideo } from '@100mslive/react-sdk';
+import { useVideo, selectIsPeerAudioEnabled, useHMSStore, selectIsPeerVideoEnabled } from '@100mslive/react-sdk';
 import './Webcam.scss';
 import { WebcamContext } from '../../../context/UserContext';
 
 const Webcam = ({ peer }) => {
-    const { fullscreen, isLocalVideoEnabled } = useContext(WebcamContext);
+    const { fullscreen } = useContext(WebcamContext);
     const { videoRef } = useVideo({
         trackId: peer.auxiliaryTracks[0] || peer.videoTrack
     });
+    
+    const isPeerAudioEnabled = useHMSStore(selectIsPeerAudioEnabled(peer.id));
+    const isPeerVideoEnabled = useHMSStore(selectIsPeerVideoEnabled(peer.id));
 
+    // console.log(peer)
+    
     return (
         <section className={`webcam ${peer.roleName === 'guest' ? 'webcam__guest' : 'webcam__host'} `}>
-            {!isLocalVideoEnabled && peer.isLocal ? 
+            <video
+            ref={videoRef}
+            autoPlay={true}
+            muted
+            className={`webcam__video ${fullscreen && peer.roleName === 'guest' ? 'webcam__mini' : fullscreen && peer.roleName === 'host' ? 'webcam__fullscreen' : ''} `}
+            playsInline 
+            />
+            {/* {!isPeerVideoEnabled ? (
                 <img className={`webcam__video webcam__image ${fullscreen && peer.roleName === 'guest' ? 'webcam__mini' : fullscreen && peer.roleName === 'host' ? 'webcam__fullscreen' : ''} `} src="https://placehold.co/180x100" alt="" /> 
-                :
-                <video
-                ref={videoRef}
-                autoPlay={true}
-                muted
-                className={`webcam__video ${fullscreen && peer.roleName === 'guest' ? 'webcam__mini' : fullscreen && peer.roleName === 'host' ? 'webcam__fullscreen' : ''} `}
-                playsInline 
-                />
-            }
+            ) : null} */}
             {!fullscreen && 
                 <article className='webcam__name'>
                     {peer.name} {peer.isLocal ? "(You)": ""}
