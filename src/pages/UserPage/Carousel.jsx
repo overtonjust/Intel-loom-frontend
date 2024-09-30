c703cae5859f06ce768935df134fdaad05e72246
marinn6 <nmarin@pursuit.org>
2024-09-30 15:15:44 -0400

    Update: Added video to UserPage Carousel

--- src/pages/UserPage/Carousel.jsx
+++ src/pages/UserPage/Carousel.jsx
@@ -6,17 +6,21 @@ import Card from '@mui/joy/Card';
 
 const data = [
   { 
-    src: 'https://images.unsplash.com/photo-1502657877623-f66bf489d236',
+    src: 'https://unsplash.com/photos/woman-in-black-tank-top-sitting-on-blue-sofa-chair-69wEv-G2W_s',
+    youtubeUrl: 'https://www.youtube.com/embed/W6NZfCO5SIk',
+    key: 1,
     // title: 'Night view',
     // description: '4.21M views',
   },
   {
     src: 'https://images.unsplash.com/photo-1527549993586-dff825b37782',
+    key: 2,
     title: 'Lake view',
     description: '4.74M views',
   },
   {
     src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
+    key: 3,
     title: 'Mountain view',
     description: '3.98M views',
   },
@@ -30,7 +34,7 @@ export default function CarouselRatio() {
         gap: 1,
         py: 1,
         overflow: 'auto',
-        width: 410,
+        width: 500,
         scrollSnapType: 'x mandatory',
         '& > *': {
           scrollSnapAlign: 'center',
@@ -39,20 +43,32 @@ export default function CarouselRatio() {
       }}
     >
       {data.map((item) => (
-        <Card orientation="horizontal" size="sm" key={item.title} variant="outlined">
-          <AspectRatio ratio="1" sx={{ minWidth: 120 }}>
+        <Card orientation="horizontal" size="sm" key={item.key} variant="outlined">
+        <AspectRatio ratio="1" sx={{ minWidth: 300 }}>
+          {item.youtubeUrl ? (
+            <iframe 
+              width="500" 
+              height="120"
+              src={item.youtubeUrl}
+              title={item.title}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          ) : (
             <img
               srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}
               src={`${item.src}?h=120&fit=crop&auto=format`}
               alt={item.title}
             />
-          </AspectRatio>
-          <Box sx={{ whiteSpace: 'nowrap', mx: 1 }}>
-            <Typography level="title-md">{item.title}</Typography>
-            <Typography level="body-sm">{item.description}</Typography>
-          </Box>
-        </Card>
-      ))}
-    </Box>
-  );
-}
+          )}
+        </AspectRatio>
+        <Box sx={{ whiteSpace: 'nowrap', mx: 1 }}>
+          <Typography level="title-md">{item.title}</Typography>
+          <Typography level="body-sm">{item.description}</Typography>
+        </Box>
+      </Card>
+    ))}
+  </Box>
+ )
+};
---
+++ src/pages/UserPage/UserPageMobile.jsx
@@ -0,0 +1,20 @@
+import {useState, useEffect, useContext} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { UserContext } from '../../context/UserContext';
+import { FaLinkedin, FaYoutube } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import './UserPageMobile.scss'
+import CarouselRatio from './Carousel';
+
+import React from 'react'
+
+const UserPageMobile = () => {
+  return (
+    <div>
+        UserPageMobile
+    </div>
+  )
+}
+
+export default UserPageMobile
