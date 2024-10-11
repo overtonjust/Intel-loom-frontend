94d0aa22f8b5f682fbab035768768d1d66668877
overtonjust <joverton@pursuit.org>
2024-10-11 17:43:00 -0400

    added button with navigation to view page

--- src/pages/instructorview/InstructorView.jsx
+++ src/pages/instructorview/InstructorView.jsx
@@ -1,7 +1,7 @@
 // Dependencies
 import React, { useEffect, useState, useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './InstructorView.scss';
 import axios from 'axios';
 import { formatDate } from '../../../utils';
@@ -12,6 +12,7 @@ import StudentCard from './components/studentCard';
 const InstructorView = () => {
     const { API } = useContext(UserContext);
     const { id } = useParams();
+    const navigate = useNavigate();
     const [lectureInfo, setLectureInfo] = useState({
         classStudents: [],
         classInfo: {},
@@ -20,9 +21,11 @@ const InstructorView = () => {
     const { classStudents, classInfo, classEnd, classStart } = lectureInfo;
     const { title } =  classInfo;
     const today = formatDate(new Date());
+    const currentTime = new Date().getHours();
     const classDay = formatDate(new Date(classStart));
     const startTime = new Date(classStart).getHours();
     const endTime = new Date(classEnd).getHours();
+    
     
 
     useEffect(() => {
@@ -35,7 +38,7 @@ const InstructorView = () => {
         <main className='lectures'>
             <header>
                 <h1 className='lectures__title'>{title}</h1>
-                <h2 className='lectures__title'><span className={classDay === today ? 'alert' : ''}>{classDay === today ? 'Today' : classDay}</span> ({startTime > 12 ? `${startTime - 12}pm` : `${startTime}am`} - {endTime > 12 ? `${endTime - 12}pm` : `${endTime}am` })</h2>
+                <h2 className='lectures__title'><span className={classDay === today ? 'alert' : ''}>{classDay === today ? 'Today' : classDay}</span> ({startTime > 12 ? `${startTime - 12}pm` : startTime === 0 ? '12am' : `${startTime}am`} - {endTime > 12 ? `${endTime - 12}pm` :  endTime === 0 ? '12am' : `${endTime}am` }) {today && (startTime - currentTime) < 1 && <button className='button-orange' onClick={() => navigate('/view')}>Join call</button>}</h2>
             </header>
             <h4 className='lectures__title'>Students enrolled:</h4>  
             <section className='lectures__students-grid'>
