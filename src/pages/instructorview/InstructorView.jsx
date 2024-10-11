98a27d97aeeb8410ed7e18f30cc295c195ade232
overtonjust <joverton@pursuit.org>
2024-10-11 16:06:22 -0400

    updated InstructorView page to list time and removed carousel

--- src/pages/instructorview/InstructorView.jsx
+++ src/pages/instructorview/InstructorView.jsx
@@ -4,10 +4,10 @@ import { UserContext } from '../../context/UserContext';
 import { useParams } from 'react-router-dom';
 import './InstructorView.scss';
 import axios from 'axios';
+import { formatTime } from '../../../utils';
 
 // Components
 import StudentCard from './components/studentCard';
-import MobileCarousel from '../../shared components/carousels/MobileCarousel';
 
 const InstructorView = () => {
     const { API } = useContext(UserContext);
@@ -18,24 +18,27 @@ const InstructorView = () => {
         title: '',
         classPictures: []
     });
-    const { classStudents, classInfo } = lectureInfo;
-    const { title, classPictures } =  classInfo;
+    const { classStudents, classInfo, classEnd, classStart } = lectureInfo;
+    const { title } =  classInfo;
     
+
     useEffect(() => {
         axios(`${API}/classes/class-date-info/${id}`, { withCredentials: true })
         .then(res => setLectureInfo(res.data))
         .catch(err => console.error(err))
     },[id]);
+    const startTime = new Date(classStart).getHours()
+    const endTime = new Date(classEnd).getHours()
 
+    console.log(lectureInfo)
     return (
         <main className='lectures'>
-            <h1 className='lectures__title'>{title}</h1>
-            <MobileCarousel imageArr={classPictures} alt={title} />  
+            <h1 className='lectures__title'>{title}: {startTime > 12 ? `${startTime - 12}pm` : `${startTime}am`} - {endTime > 12 ? `${endTime - 12}pm` : `${endTime}am` }</h1>
             <h3 className='lectures__title'>Students enrolled:</h3>  
             <section className='lectures__students-grid'>
                 {classStudents.map(student => (
                     <StudentCard key={student.userId} studentInfo={student}/>
-                ))}       
+                ))}     
             </section>
         </main>
     );
