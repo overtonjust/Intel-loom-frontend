29e85dfb27720e07211dd61749ab5c87c803e0c9
overtonjust <joverton@pursuit.org>
2024-10-11 12:15:33 -0400

    Set up lectures page view and StudentCard

--- src/pages/instructorview/InstructorView.jsx
+++ src/pages/instructorview/InstructorView.jsx
@@ -1,14 +1,23 @@
 // Dependencies
 import React from 'react';
+import './InstructorView.scss';
 
 // Components
 import StudentCard from './components/studentCard';
 
 const InstructorView = () => {
     return (
-        <div>
-            <StudentCard/>
-        </div>
+        <main className='lectures'>
+            {/* Insert Class title w/ Date */}
+            {/* Insert carosel component */}        
+            <section className='lectures__students-grid'>
+            {/* Map through students to make cards once data is received */}        
+                <StudentCard/>
+                <StudentCard/>
+                <StudentCard/>
+                <StudentCard/>
+            </section>
+        </main>
     );
 };
 
--- src/pages/instructorview/components/StudentCard.jsx
+++ src/pages/instructorview/components/StudentCard.jsx
@@ -1,17 +1,21 @@
 // Dependencies
 import React from 'react';
 import './StudentCard.scss';
+import { useNavigate } from 'react-router-dom';
 
 // Components
 import defaultImg from '../../../assets/default-profile.png';
 
 const StudentCard = () => {
+    const navigate = useNavigate();
+    // const { userId, name, email, profilePicture } = userInfo;
+
     return (
-        <article className='student-card'>
+        <article className='student-card' onClick={() => navigate(`/profile/3`)}>
             <img className='student-card__img' src={defaultImg} alt="" />
             <div className='student-card__col'>
-                <h5 className='student-card__name'>name</h5>    
-                <p className='student-card__email'>email</p>
+                <p className='student-card__name'>Justin Overton</p>    
+                <p className='student-card__email'>joverton@pursuit.org</p>
             </div>
         </article>
     );
