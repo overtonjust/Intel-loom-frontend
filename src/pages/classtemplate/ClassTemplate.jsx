fdd308c545b33128761ab295fc17d8341c188188
Marco Quispe <mquispe@pursuit.org>
2024-10-15 17:18:29 -0400

    Started class template page

--- src/pages/classtemplate/ClassTemplate.jsx
+++ src/pages/classtemplate/ClassTemplate.jsx
@@ -1,4 +1,20 @@
+import { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import axios from "axios";
+
 const ClassTemplate = () => {
+  const { API } = useContext(UserContext);
+  const { id } = useParams();
+  const [classData, setClassData] = useState(null);
+  console.log(classData);
+
+  useEffect(() => {
+    axios.get(`${API}/instructors/instructor-class-template-by-id/${id}`, { withCredentials: true })
+      .then(res => setClassData(res.data))
+      .catch(err => console.log(err));
+  }, [id]);
+
   return (
     <div>
       <h1>Class Template</h1>
