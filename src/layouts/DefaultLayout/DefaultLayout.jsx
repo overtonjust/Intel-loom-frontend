e6e98537586039db34dcbfa54fd16bb2e4962b48
Marco Quispe <mquispe@pursuit.org>
2024-10-28 12:01:36 -0400

    Finish responsive view

--- src/layouts/DefaultLayout/DefaultLayout.jsx
+++ src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -1,6 +1,7 @@
 // Dependencies
 import "./DefaultLayout.scss";
 import { useState, useEffect, useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 import { Outlet } from "react-router-dom";
 
 // Contexts
@@ -11,6 +12,7 @@ import Footer from "./components/Footer";
 import MobileNav from "./components/MobileNav";
 import Brand from "./components/Brand";
 import PopUp from "./components/PopUp";
+import DesktopNav from "./components/DesktopNav";
 
 // Hooks
 import useSessionChecker from "./hooks/useSessionChecker";
@@ -32,32 +34,63 @@ const DefaultLayout = () => {
   }, [user]);
 
   useEffect(() => {
-    if(shouldScroll) {
+    if (shouldScroll) {
       scrollRef.current?.scrollTo({
         top: 0,
-        behavior: 'smooth'
-      })
-      setShouldScroll(false)
+        behavior: "smooth",
+      });
+      setShouldScroll(false);
     }
-  }, [shouldScroll])
+  }, [shouldScroll]);
 
   useSessionChecker(API, setUser, user);
-  
+
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+  const fitsOneColumn = useMediaQuery({ query: "(max-width: 550px)" });
+  const fitsTwoColumns = useMediaQuery({ query: "(max-width: 900px)" });
+  const fitsThreeColumns = useMediaQuery({ query: "(max-width: 1280px)" });
+
   return (
-    <UserContext.Provider value={{ user, setUser, setMessage, API, setShouldScroll }}>
-      <article className="default-mobile-layout">
-        <header className="default-mobile-layout__header">
-          <Brand />
-        </header>
-        <section ref={scrollRef} className="default-mobile-layout__content">
-          <Outlet />
-          <Footer />
-          {message && <PopUp message={message} setMessage={setMessage} />}
-        </section>
-        <footer className="default-mobile-layout__footer">
-          <MobileNav />
-        </footer>
-      </article>
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        setMessage,
+        API,
+        setShouldScroll,
+        isTabletOrMobile,
+        fitsOneColumn,
+        fitsTwoColumns,
+        fitsThreeColumns,
+      }}
+    >
+      {isTabletOrMobile ? (
+        <article className="default-mobile-layout">
+          <header className="default-mobile-layout__header">
+            <Brand />
+          </header>
+          <section ref={scrollRef} className="default-mobile-layout__content">
+            <Outlet />
+            <Footer />
+            {message && <PopUp message={message} setMessage={setMessage} />}
+          </section>
+          <footer className="default-mobile-layout__footer">
+            <MobileNav />
+          </footer>
+        </article>
+      ) : (
+        <article className="default-desktop-layout">
+          <header className="default-desktop-layout__header">
+            <Brand />
+            <DesktopNav user={user} />
+          </header>
+          <section ref={scrollRef} className="default-desktop-layout__content">
+            <Outlet />
+            <Footer />
+            {message && <PopUp message={message} setMessage={setMessage} />}
+          </section>
+        </article>
+      )}
     </UserContext.Provider>
   );
 };
---
+++ src/layouts/DefaultLayout/components/DesktopNav.jsx
@@ -0,0 +1,35 @@
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import "./DesktopNav.scss";
+
+const DesktopNav = ({ user }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isNotLoginPage = location.pathname !== "/login";
+
+  return (
+    <nav className="desktop-nav">
+      <Link className="nav-link" to="/">
+        Home
+      </Link>
+      {user ? (
+        <>
+          <Link className="nav-link" to="/myclasses">
+            Classes
+          </Link>
+          <Link className="nav-link" to="/forums">
+            Forums
+          </Link>
+          <Link className="nav-link" to={`/profile/${user.userId}`}>
+            Profile
+          </Link>
+        </>
+      ) : isNotLoginPage ? (
+        <button className="button-orange" onClick={() => navigate("/login")}>
+          Login
+        </button>
+      ) : null}
+    </nav>
+  );
+};
+
+export default DesktopNav;
